689b18fecd3cd34eaccb967f67a10a20f9f8f3b2
Genevieve Conty <gmireillec@guest-2-00536.dhcp.unc.edu>
2016-09-08 20:45:04 -0400

    navbar

--- assets/javascript/mapPage.js
+++ assets/javascript/mapPage.js
@@ -9,7 +9,18 @@ $('select').material_select();
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year
-    format: 'dd-mm-yyyy' });
+    format: 'mm-dd-yyyy' });
+
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    }
+  );
 
 
 });
